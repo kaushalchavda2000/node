9f37a993c5503c4e12cace8ad3264265c2aff91d
kaushal chavda <kaushal@york.ie>
2022-12-31 15:17:49 +0530

    install some packages and created server.

---
+++ app.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: "kaushalChavda",
+  age: 22,
+  address: {
+    city: "botad",
+    state: "gujrat",
+    country: "india",
+  },
+  friends: ["kaushik", "ajay", "kuldip", "hasit", "harsh", "nayan"],
+};
--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Starting");
+// const fs = require("fs");
+// const app = require("./app");
+
+// fs.writeFileSync("demo.txt", "this is the first file.");
+
+// const friend = app.friends.filter((item) => item === "kaushik");
+// console.log(friend);
+
+// for (let index = 0; index < 10; index++) {
+//   fs.writeFileSync(`demo${index}.txt`, `this is the ${index} file.`);
+// }
+const http = require("http");
+
+const server = http
+  .createServer((req, res) => {
+    res.write("hello this is the");
+    res.end();
+  })
+  .listen(3000);
